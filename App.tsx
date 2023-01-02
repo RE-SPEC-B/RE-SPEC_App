c5ebdf23b55f3e84e61cea90fd55e056fe6be38a
hoooooou-jun <jipkim2@gmail.com>
2023-01-02 22:53:55 +0900

    Feature: 네비게이션 연결

--- App.tsx
+++ App.tsx
@@ -1,40 +1,14 @@
 import React from "react";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
-
-//세팅 시험용
-import { Fonts } from "@styles/font";
-import SvgIcon from "@components/svg/SvgIcon";
-import ErrorBoundary from "@components/errorBoundary/ErrorBoundary";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "@navigations/RootNavigator";
-// import { LOAD_STORYBOOK } from "@env";
 
 const LOAD_STORYBOOK = false;
 
 const App = () => {
-  const Font = Fonts();
   return (
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
-    // <ErrorBoundary fallback={<View></View>}>
-    //   <SafeAreaView>
-    //     <ScrollView contentInsetAdjustmentBehavior="automatic">
-    //       <View>
-    //         <Text style={Font.title1}>테스트</Text>
-    //         <Text style={Font.title2}>테스트</Text>
-    //         <Text style={Font.body1}>테스트</Text>
-    //         <Text style={Font.body2}>테스트</Text>
-    //         <Text style={Font.body3}>테스트</Text>
-    //         <SvgIcon name={"add"} />
-    //         <SvgIcon name={"communityOff"} />
-    //         <SvgIcon name={"homeOff"} />
-    //         <SvgIcon name={"homeOn"} />
-    //         <SvgIcon name={"pen"} />
-    //       </View>
-    //     </ScrollView>
-    //   </SafeAreaView>
-    // </ErrorBoundary>
   );
 };
 
