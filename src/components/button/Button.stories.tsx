a0fbc0e9af5e8ada7234159f196edf97a3b2e5a5
injulme <injulme0309@naver.com>
2023-01-04 15:33:08 +0900

    컴포넌트 title -> label 로 변경

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -2,5 +2,5 @@ import { storiesOf } from "@storybook/react-native";
 import Button from "./Button";
 
 storiesOf("Button", module)
-  .add("main", () => <Button title="button" disabled={false} />)
-  .add("disabled", () => <Button title="button" disabled={true} />);
+  .add("main", () => <Button label="button" disabled={false} />)
+  .add("disabled", () => <Button label="button" disabled={true} />);
