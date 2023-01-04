41abaf0c42754a2acd84a7f1091f068e54209ad9
Byeongjae Yu <87460510+byeongjaeyu@users.noreply.github.com>
2023-01-04 10:49:05 +0900

    feat: tag component

--- src/components/tag/Tag.stories.tsx
+++ src/components/tag/Tag.stories.tsx
@@ -1,4 +1,6 @@
 import { storiesOf } from "@storybook/react-native";
 import Tag from "./Tag";
 
-storiesOf("Tag", module).add("label", () => <Tag label="zz" />);
+storiesOf("Tag", module)
+  .add("label", () => <Tag label="zz" />)
+  .add("withEmoji", () => <Tag label="동문보기" emoji="🙌" />);
