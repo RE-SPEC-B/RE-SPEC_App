f892c89be71e1ceee44a47729e456fb0e586cff3
JeongYunMi <tamd5971@gmail.com>
2023-01-05 20:41:52 +0900

    fix: 디데이 카운트 날짜 에러 수정

--- src/utils/dateCounting.ts
+++ src/utils/dateCounting.ts
@@ -1,9 +1,9 @@
 const dateCounting = (endDate:Date):number => {
     const now = new Date();
     const currDay = 24 * 60 * 60 * 1000;
-    const diff = +endDate - +now;
+    const diff = endDate.getTime() - now.getTime();
 
-    return Math.floor(diff/currDay);
+    return Math.floor(diff/currDay)+1;
 }
 
 export { dateCounting };
