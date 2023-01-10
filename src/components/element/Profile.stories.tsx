baf59c0d6275adec942ca690052f6bffab5693b3
JeongYunMi <tamd5971@gmail.com>
2023-01-10 19:11:11 +0900

    feat: default 프로필 컴포넌트 구현 및 스토리 작성

--- src/components/element/Profile.stories.tsx
+++ src/components/element/Profile.stories.tsx
@@ -1,21 +1,12 @@
 import { storiesOf } from "@storybook/react-native";
-import { EGradientProfile } from "./GradientDefine";
 import Profile from "./Profile";
-import ProfileEdit from "./ProfileEdit";
 
-const data =
-  "https://cdn.pixabay.com/photo/2022/12/28/21/10/night-7683839_640.jpg";
-const data2 =
-  "https://cdn.pixabay.com/photo/2022/09/22/08/09/halloween-7471880__480.png";
+const data = 'https://cdn.pixabay.com/photo/2022/12/28/21/10/night-7683839_640.jpg';
+const data2 = 'https://cdn.pixabay.com/photo/2022/09/22/08/09/halloween-7471880__480.png';
 
 storiesOf("profile", module)
-  .add("profile1", () => (
-    <Profile imageUri={data} level={EGradientProfile.LEVEL_4} />
-  ))
-  .add("profile2", () => (
-    <Profile imageUri={data2} level={EGradientProfile.LEVEL_4} />
-  ))
-  .add("profile3", () => <Profile />);
-storiesOf("profileEdit", module)
-  .add("ProfileEdit1", () => <ProfileEdit imageUri={data2} />)
-  .add("ProfileEdit2", () => <ProfileEdit />);
+  .add("profile1", () => <Profile imageUri={data} />)
+storiesOf("profile", module)
+  .add("profile2", () => <Profile imageUri={data2} />)
+storiesOf("profile", module)
+  .add("profile3", () => <Profile imageUri='' />)
--- src/components/element/Profile.tsx
+++ src/components/element/Profile.tsx
@@ -1,48 +1,40 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import {
+    View,
+    Image,
+    StyleSheet 
+  } from "react-native";
 import { EColor } from "@styles/color";
-import Gradient from "./Gradient";
-import { EGradientProfile } from "./GradientDefine";
 
-interface IProfile {
-  level?: EGradientProfile;
-  imageUri?: string;
-}
-
-/**
- *
- * @param level
- * EGradientProfile enum값 사용.
- */
-const Profile = (props: IProfile) => {
-  const {
-    imageUri = "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792__480.png",
-    level = EGradientProfile.NONE,
-  } = props;
-  return (
-    <View style={styles.container}>
-      <Gradient level={level}>
-        <Image
-          style={styles.image}
-          source={{ uri: imageUri }}
-          resizeMode="cover"
-        />
-      </Gradient>
-    </View>
-  );
+const Profile = ({imageUri}: {imageUri:string|any|null}) => {
+    return (
+      <View style={styles.container} >
+        {imageUri !== '' && (
+          <Image 
+            style={styles.image}
+            source={{uri: imageUri}}
+            resizeMode="cover"
+          />
+        )}
+        
+      </View>
+    );
 };
 
 export default Profile;
 
 const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: EColor.GRAY_100,
-    width: 140,
-    height: 140,
-  },
-  image: {
-    width: 136,
-    height: 136,
-    borderRadius: 70,
-  },
-});
+    container:{
+      backgroundColor: EColor.GRAY_100,
+      width: 140,
+      height: 140,
+      borderRadius: 70,
+    },
+    image: {
+      width: 140,
+      height: 140,
+      borderRadius: 70,
+      borderWidth: 2,
+      borderColor: EColor.GRAY_300
+    },
+  });
