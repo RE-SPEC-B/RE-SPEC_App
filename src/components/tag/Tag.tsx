7c3951a725c738f0a4ce746b9726f40c6306579c
Byeongjae Yu <87460510+byeongjaeyu@users.noreply.github.com>
2023-01-10 17:46:28 +0900

    feat: tag component 확장
    사용 용도에 따라 스타일이 다른 tag컴포넌트 size props로 제어

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,11 +1,13 @@
 import SvgIcon from "@components/svg/SvgIcon";
-import { EBgColor, EBrandColor, EFontColor } from "@styles/color";
+import { EColor } from "@styles/color";
 import { Fonts } from "@styles/font";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { SvgIconProps } from "@components/svg/SvgIcon";
+import tagStyles from "./styles";
 
-interface ITagProps {
+export interface ITagProps {
+  size?: "small" | "big";
   onChange?: () => void;
   selected?: boolean;
   emoji?: string;
@@ -13,33 +15,20 @@ interface ITagProps {
   svg?: SvgIconProps["name"];
 }
 
-const styles = StyleSheet.create({
-  tagWrapper: {
-    alignSelf: "flex-start",
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderRadius: 100,
-    borderWidth: 1,
-    // width: "auto",
-  },
-  emoji: {
-    fontSize: 14,
-  },
-  labelWithEmoji: {
-    marginLeft: 6,
-  },
-});
-
+/**
+ * 직무태그, 검색태그일 경우 size = "big"
+ */
 const Tag = (props: ITagProps) => {
   const {
+    size = "small",
     onChange = () => console.log("tag!"),
     selected = false,
     emoji,
     label,
     svg,
   } = props;
+
+  const styles = tagStyles(size);
   const [isSelected, setIsSelected] = useState<boolean>(selected);
   useEffect(() => {
     onChange();
@@ -48,20 +37,18 @@ const Tag = (props: ITagProps) => {
     <TouchableOpacity
       style={[
         styles.tagWrapper,
-        isSelected
-          ? {
-              backgroundColor: "#E5FCFD",
-              borderColor: EBrandColor.MAIN_TEXT,
-            }
-          : {
-              backgroundColor: "#FFFFFF",
-              borderColor: "#CCCCCC",
-            },
+        isSelected ? styles.onSelectedTag : styles.offSelectedTag,
       ]}
       onPress={() => setIsSelected((state) => !state)}
     >
       <Text style={styles.emoji}>{emoji}</Text>
-      <Text style={[emoji && styles.labelWithEmoji, Fonts().body3]}>
+      <Text
+        style={[
+          emoji && styles.labelWithEmoji,
+          Fonts().body3,
+          isSelected ? styles.onSelectedLabel : styles.offSelectedLabel,
+        ]}
+      >
         {label}
       </Text>
       {svg && <SvgIcon name={svg} />}
