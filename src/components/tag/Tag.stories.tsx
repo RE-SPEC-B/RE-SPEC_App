78ee636be2127ef58caec088cadd4e45a7a7abf6
Byeongjae Yu <87460510+byeongjaeyu@users.noreply.github.com>
2023-01-10 17:46:28 +0900

    feat: tag component 확장
    사용 용도에 따라 스타일이 다른 tag컴포넌트 size props로 제어

--- src/components/tag/Tag.stories.tsx
+++ src/components/tag/Tag.stories.tsx
@@ -3,5 +3,6 @@ import Tag from "./Tag";
 
 storiesOf("Tag", module)
   .add("label", () => <Tag label="zz" />)
+  .add("labelBig", () => <Tag size="big" label="#국내MBA" />)
   .add("withEmoji", () => <Tag label="동문보기" emoji="🙌" />)
   .add("withSvg", () => <Tag svg={"filter"} />);
